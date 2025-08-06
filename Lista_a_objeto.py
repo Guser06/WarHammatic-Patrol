9f4bc8a8c9786f554f75cb6310ca1808b4da72da
Guser06 <gusegus@hotmail.com>
2025-08-06 15:27:31 -0600

    Creador de objetos terminado

--- Lista_a_objeto.py
+++ Lista_a_objeto.py
@@ -1,28 +1,30 @@
+import json
 
-lista1 = ['''Aqui va la lista sacada del JSON no 1''']
-lista2 = ['''Aqui va la lista sacada del JSON no 2''']
-Ejercitos_listas = [lista1, lista2]#Listas provenientes de los JSON
-Ejercitos_objetos =[]#Lista donde se guardaran los ejercitos convertidos en objetos de Python
+with open('UM_patrol.json', 'r') as file:
+    diccionario1 = json.load(file)
+
+diccionario2 = [] #Aqui va el diccionario sacada del JSON no 2
+Ejercitos_diccionarios = [diccionario1, diccionario2] #Diccionarios provenientes de los JSON
+Ejercitos_objetos =[]   #Lista donde se guardaran los ejercitos convertidos en objetos de Python
 
 StatsTx = ["Movimiento", "Resistencia", "Salvación",
-           "Heridas", "Liderazgo", "Control de objetivo"]#Nombre de las stats de las miniaturas
+           "Heridas", "Liderazgo", "Control de objetivo"]   #Nombre de las stats de las miniaturas
 
 ArmaTx = ["Nombre", "Alcance", "No. de Ataques",
-          "Habilidad", "Fuerza", "Perforación", "Daño"]#Nombre de las stats de las armas
+          "Habilidad", "Fuerza", "Perforación", "Daño"] #Nombre de las stats de las armas
     
 class Individuo:
-    def __init__(self, lista):
-        self.nombre = lista[0]#Nombre de la miniatura
-        self.stats_base = dict(zip(StatsTx, lista[1])) #Stats de la miniatura
-        self.rango1 = dict(zip(ArmaTx, lista[2]))  # Armas de rango
-        self.rango2 = dict(zip(ArmaTx, lista[3]))
-        self.rango3 = dict(zip(ArmaTx, lista[4]))
-        self.rango4 = dict(zip(ArmaTx, lista[5]))
-        self.mele1 = dict(zip(ArmaTx, lista[6])) # Armas cuerpo a cuerpo
-        self.mele2 = dict(zip(ArmaTx, lista[7]))
-        self.habilidades = [lista[8]]#Claves y "habilidades del lado del datasheet"
-        self.dmg = 0 #Daño recibido por la miniatura
-        self.vivo = True#La miniatura esta viva
+    def __init__(self, diccionario):
+        self.nombre = diccionario.get("Nombre")    #Nombre de la miniatura
+        self.stats_base = dict(zip(StatsTx, diccionario.get("Stats"))) #Stats de la miniatura
+        self.rango1 = dict(zip(ArmaTx, diccionario.get("Rango1"))) if diccionario.get("Rango1") is not None else None # Armas de rango
+        self.rango2 = dict(zip(ArmaTx, diccionario.get("Rango2"))) if diccionario.get("Rango2") is not None else None
+        self.rango3 = dict(zip(ArmaTx, diccionario.get("Rango3"))) if diccionario.get("Rango3") is not None else None
+        self.rango4 = dict(zip(ArmaTx, diccionario.get("Rango4"))) if diccionario.get("Rango4") is not None else None
+        self.mele1 = dict(zip(ArmaTx, diccionario.get("Mele1"))) if diccionario.get("Mele1") is not None else None    # Armas cuerpo a cuerpo
+        self.mele2 = dict(zip(ArmaTx, diccionario.get("Mele2"))) if diccionario.get("Mele2") is not None else None
+        self.dmg = 0    #Daño recibido por la miniatura
+        self.vivo = True    #La miniatura esta viva
 
     def recibir_dano(self, dano):
         self.dmg += dano
@@ -35,16 +37,16 @@ class Individuo:
         return f"{self.nombre} ({estado})"
 
 class Unidad:
-    def __init__(self, lista):
+    def __init__(self, diccionario):
         self.mov = 0
         self.atk = 0
         self.shock = False
         self.miembros = []
-        self.nombre = lista[0]
-        self.lider = lista[1]
-        self.habilidades = lista[2]
-        self.claves = lista[3]
-        self.nm = lista[-1]
+        self.nombre = diccionario.get("Nombre")
+        self.lider = ''
+        self.habilidades = diccionario.get("Habilidades")
+        self.claves = diccionario.get("Claves")
+        self.nm = diccionario.get("Numero Miniaturas")
 
     def eliminar_muertos(self):
         self.miembros = [
@@ -54,21 +56,29 @@ class Unidad:
         return f"{self.nombre}:\n" + "\n".join(str(miembro) for miembro in self.miembros)
 
 class Ejercito:
-    def __init__(self, lista):
+    def __init__(self, diccionario):
         self.pc = 0
         self.pv = 0
-        self.faccion = lista[-1]
-        self.nu = lista[-2]
+        self.faccion = diccionario.get('Faccion')
+        self.nu = diccionario.get('Numero Unidades')
         self.unidades = []
 
     def __repr__(self):
         return f"{self.faccion}:\n" + "\n".join(str(unidad) for unidad in self.unidades)
 
-m = 0
-for e in Ejercitos_listas:
-    Ejercitos_objetos[m] = Ejercito(e)
-    for u in range(e[m][-1]):
-        Ejercitos_objetos[m].unidades[u] = Unidad(e[m][u])
-        for i in range(e[m][u][-1]):
-            Ejercitos_objetos[m].unidades[u].miembros[i] = Individuo(e[m][u][i])
-    m+1
+
+i = 0
+j = 0
+for d in Ejercitos_diccionarios:
+    if isinstance(d, dict):
+        Ejercitos_objetos.append(Ejercito(d))
+        for cu, vu in d.items():
+            if isinstance(vu, dict):
+                Ejercitos_objetos[i].unidades.append(Unidad(vu))
+                for cm, vm in vu.items():
+                    if isinstance(vm, dict):
+                        Ejercitos_objetos[i].unidades[j].miembros.append(Individuo(vm))
+                j + 1
+        i + 1
+        
+print(Ejercitos_objetos[0])
